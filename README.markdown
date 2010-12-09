41176becf8a12706c75329065c7ec85932cafcc1
kurita@mac-ubuntu <aphd.kurita@gmail.com>
2010-12-09 04:17:18 -0800

    色々オプション追加

--- README.markdown
+++ README.markdown
@@ -4,6 +4,8 @@ EasyCrossFade
 クロスフェードするスライドショーの簡易版
 (要JQuery)
 
+全然簡易版じゃないよね
+
 Version
 -------
 
@@ -15,26 +17,31 @@ Usage
 easy_crossfade.jsを読み込んでください
 
 HTMLにEasyCrossFadeの設定を行います。
-<script type="text/javascript">
-//<![CDATA[
-    EasyCrossFade.init({
-      // スライドショーさせるオブジェクト
-      element: "#flash",
-      
-      // 画像ディレクトリの指定
-      imageDir: "images/",
-
-      // 1度しか表示されないスライド
-      catchImages: ["flash-catch1.jpg", "flash-catch2.jpg"],
-
-      // 繰り返し表示されるスライド
-      loopImages: ["flash-loop.jpg", "flash-loop.jpg", "flash-loop.jpg"],
-
-      // スライドのスピード
-      speed: 3000
-    });
-//]]>
-</script>
+    <script type="text/javascript">
+      //<![CDATA[
+      //第一引数: スライドショーを表示する要素id
+      //第二引数: オプション
+      EasyCrossFade.init("#flash", {
+        // 幅
+        width: "360px"
+        // 高さ
+        height: "240px"
+        // 画像ディレクトリの指定
+        imageDir: "images/",
+        // 1度しか表示されないスライド
+        catchImages: ["flash-catch1.jpg", "flash-catch2.jpg"],
+        // 繰り返し表示されるスライド
+        loopImages: [
+          "flash-loop.jpg",
+          ["flash-loop.jpg", "http://example.com/"] //配列でリンクを指定
+        ],
+        // スライドのスピード
+        slideSpeed: 3000,
+        // フェードのスピード
+        fadeSpeed: 3000,
+      });
+    //]]>
+    </script>
 
 スライドする画像の順番は、catchImages[0], ... catchImages[n-1], loopImages[0], ... loopImages[n-1], loopImages[0], ... のような順番で表示されます。
 
@@ -42,6 +49,5 @@ Todo
 ----
 
 + ロード中の画像を指定できる機能
-+ 画像の切り替えをスムーズにする
  
 copyright 2010 waco, released under the MIT license 
--- easy_crossfade.js
+++ easy_crossfade.js
@@ -2,71 +2,102 @@
 EasyCrossFade
 Copyright (c) 2010 waco, released under the MIT license
 requre: JQuery (any version)
+version: 0.0.1
 */
 
 // settings
 var EasyCrossFade = (function(){
   // element to crossfade
-  var element = "#flash";
+  var elementObject;
   
   // crossfade images
-  var imageDir = "images/";
+  var imageDir = "";
   var catchImages = [];
-  var loopImages = ["loop.jpg"];
+  var loopImages = [];
 
   // crossfade speed (msec)
-  var speed = 1000;
+  var slideSpeed = 5000;
+  var fadeSpeed = 1000;
   
   var imageLength = 0;
   var imageLoopPosition = 0;
   var imageIndex = 0;
   var crossFadeImages = [];
   var timer;
+  var imageLoadedFlag = false;
     
   var preload = function(){
     var cacheImageSrc = [];
     var loadedImageCounts = 0;
     $(crossFadeImages).each(function(){
       var image = new Image();
-      image.src = imageDir + this;
+      image.src = this.path;
       cacheImageSrc.push(image);
       $(image).load(function(){
         loadedImageCounts++;
         if(loadedImageCounts == imageLength){
-          startCrossFade();
+          imageLoadedFlag = true;
         }
       });
     });
   }
+  var startCrossFade = function(){
+    if(imageLoadedFlag){
+      crossFade();
+    }
+    else{
+      setTimeout(startCrossFade, 500);
+    }
+  }
   var crossFade = function(){
-      var elementObject = $(element);
-      elementObject.fadeOut("fast", function(){
-        elementObject.css("background",
-          "transparent url(" + imageDir + crossFadeImages[imageIndex] + ") no-repeat center center");
-        elementObject.fadeIn("fast");
+      var nextFrameBackground = "transparent url(" + crossFadeImages[imageIndex].path + ") no-repeat center center";
+      nextFrame = $("<div></div>").css({
+        width: "100%", height: "100%",
+        background: nextFrameBackground
+      }).hide();
+      elementObject.empty().append(nextFrame);
+
+      nextFrame.animate({opacity: "show"}, fadeSpeed, "linear", function(){
+        elementObject.css("background", nextFrameBackground).empty();
+        
+        if(crossFadeImages[imageIndex].link){
+            elementObject.append('<a href="' + crossFadeImages[imageIndex].link +
+              '" style="display: block; width: 100%; height: 100%;"></a>');
+        }
         imageIndex++;
         if(imageIndex >= imageLength) imageIndex = imageLoopPosition;
       });
-  }
-  var startCrossFade = function(){
-    crossFade();
-    timer = setInterval(crossFade, speed);
+      setTimeout(crossFade, slideSpeed);
   }
   
   return {
-    init: function(params){
-      if(params.element) element = params.element;
+    init: function(element, params){
+      if(!element) return;
+      
       if(params.imageDir) imageDir = params.imageDir;
       if(params.catchImages) catchImages = params.catchImages;
       if(params.loopImages) loopImages = params.loopImages;
-      if(params.speed) speed = params.speed;  
+      if(params.slideSpeed) slideSpeed = params.slideSpeed;
+      if(params.fadeSpeed) fadeSpeed = params.fadeSpeed;
   
-      crossFadeImages = catchImages.concat(loopImages);
+      crossFadeImages = $(catchImages.concat(loopImages)).map(function(){
+        var path, link;
+        if(this instanceof Array){ path = this[0]; link = this[1]; }
+        else { path = this; }
+        return { path: imageDir + path, link: link }
+      });
+      
       imageLoopPosition = loopImages.length - 1;
       imageLength = crossFadeImages.length;
       
+      preload();
+      
       $(document).ready(function(){
-        preload();
+        elementObject = $(element);
+        if(params.width) elementObject.width(params.width);
+        if(params.height) elementObject.height(params.height);
+        
+        startCrossFade();
       });
     }
   }
